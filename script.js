77c167e8d8cce5037d896690acfdc51612717bd1
Cordozar <cant09y@gmail.com>
2022-11-24 18:48:56 +0300

    Done part 2

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+do {
+  numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+} while (!numberOfFilms || isNaN(numberOfFilms) || numberOfFilms.includes(' '));
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -10,8 +28,28 @@ const personalMovieDB = {
   privat: false,
 };
 
-for (let i = 0; i < 2; i += 1) {
-  const watchedFilm = prompt('Один из последних просмотренных фильмов?', '');
-  const assessmentFilm = prompt('На сколько оцените его?');
-  personalMovieDB.movies[watchedFilm] = assessmentFilm;
+if (personalMovieDB.count < 10 && personalMovieDB.count >= 0) {
+  alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  alert('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+  alert('Вы киноман');
+} else {
+  alert('Произошла ошибка');
 }
+
+for (let i = 0; i < 2; i += 1) {
+  let watchedFilm;
+  let assessmentFilm;
+  do {
+    watchedFilm = prompt('Один из последних просмотренных фильмов?', '');
+  } while (!watchedFilm || watchedFilm.length > 50 || watchedFilm.includes(' '));
+  do {
+    assessmentFilm = prompt('На сколько оцените его?');
+  } while (!assessmentFilm || isNaN(assessmentFilm) || assessmentFilm.includes(' '));
+  personalMovieDB.movies[watchedFilm] = +assessmentFilm;
+}
+
+console.log(personalMovieDB);
+
+
